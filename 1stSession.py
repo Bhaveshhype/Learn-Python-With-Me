d4d172a848f4088651bf43ae5f63cf7bbb9aeee2
Bhavesh Patil <123001201+Bhaveshhype@users.noreply.github.com>
2026-01-06 21:10:03 +0530

    Uncomment and execute Python code in 1stSession.py

---
+++ 1stSession.py
@@ -0,0 +1,68 @@
+print("hello World")
+
+name = "om"
+age = 25
+salary = 23000
+print("Om Profile is" ,name, age, salary)
+
+c= 2+3j
+print(c)
+
+deci = 22.72
+print(deci)
+
+tuplet=(1,44,"python Symbiot")
+
+d = {22,44,66}
+print(d)
+print(d)
+
+listing = [1,44,"lagan set"]
+print(listing)
+
+g = {"name","rao"}
+print(g)
+
+P = int(input("Enter the first5 number"))
+Q = int(input("Enter the Second number"))
+Sum = P + Q
+print("Sum of numbers is:",Sum)
+
+#operators
+A = int(input("Enter the first number"))
+B = int(input("Enter the second number"))
+ADD = A + B
+SUB = A - B
+Mul = A * B
+Div = A / B
+floordiv = A // B #roundup value of division
+i = A%B
+print(A>B)  #comparision  #output ; False
+print(A<B)     #output ; True
+print(A==B)    #output ; False
+print(A!=B)    #output ; True
+print(B != A)  #output ; True
+
+print("The Addition is:",ADD)
+print("The Subtraction is:",SUB)
+print("The Multiplication is:",Mul)
+print("The Division is:",Div)
+print("The Floor Division is:",floordiv)
+print( "the modulus is", i )
+
+Assignment
+Q = int(input("Enter the First number"))
+P = int(input("Enter the Second number"))
+Q+=3
+Q-=3
+Q*=4
+Q/=5
+Q**=7
+Q%=8
+print(Q)
+
+
+
+
+
+
