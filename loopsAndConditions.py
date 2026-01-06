aaa92a349b3714da85abe6b8cd586a4876d1d0db
Bhavesh Patil <123001201+Bhaveshhype@users.noreply.github.com>
2026-01-06 21:20:26 +0530

    Refactor loops and conditions in loopsAndConditions.py

---
+++ loopsAndConditions.py
@@ -0,0 +1,76 @@
+i=0
+for i in range(1,100):
+    print(i)
+
+
+i =0
+for i in range(1,100):
+    if i % 2 == 0:
+        print("the number is even",i)
+
+x=0
+for x in range (10):
+    print("Hello World")
+    x=x+1
+    print(x)
+
+x=0
+for x in range (0,9):
+    print("Natural Number")
+    x=x+1
+    print(x)
+
+i=0
+for i in range(0,10):
+    i=i+1
+    print(i)
+
+sum = 0
+for i in range(1,101):
+    sum += i
+print(sum)
+
+i=0
+for i in range (125):
+    print(i)
+    if i % 3 and i % 7:
+        print("the number is divisible by 3 and 7")
+    elif i % 3 :
+        print("the number is divisible by 3")
+    elif i % 7 :
+        print("the number is divisible by 7")
+    else:
+        print("the number is not divisible by 3 and 7")
+
+for i in range(1,11):
+    print("5 X", i ,"=",i*5)
+
+import math
+i=0
+for i in range(1,11):
+    print(i,":- Square root is",math.sqrt(i))
+
+
+
+sum of even numbers
+sum=0
+for i in range(2,21,2):
+    sum += i
+print(sum)
+
+# from math import factorial
+import math
+import factorial
+n=5
+factorial=1
+for i in range(1,n+1):
+    factorial=factorial*i
+    print(factorial)
+
+n=10
+factorial=1
+for i in range(1,n+1):
+    factorial=factorial*i
+    print(factorial)
+
+
